c59b1774dc3f0d427ef72d3543bb0561b2891a5c
lugalu <luizjaf01@gmail.com>
2024-12-06 09:18:06 -0300

    feat: Initial Setup

---
+++ TaxiApp/TaxiAppApp.swift
@@ -0,0 +1,12 @@
+//Created by Lugalu on 06/12/24.
+
+import SwiftUI
+
+@main
+struct TaxiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TaxiAppTests/TaxiAppTests.swift
@@ -0,0 +1,12 @@
+//Created by Lugalu on 06/12/24.
+
+import Testing
+@testable import TaxiApp
+
+struct TaxiAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
